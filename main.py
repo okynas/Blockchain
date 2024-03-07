5d3a53abab3733463fe04593be7228537a038f81
OkYNaS <36879451+okynas@users.noreply.github.com>
2024-03-07 17:02:38 +0100

    Update main.py with black formating

--- main.py
+++ main.py
@@ -1,74 +1,84 @@
-from flask import Flask, jsonify, request
 from uuid import uuid4
+
+from flask import Flask, jsonify, request
+
 from classes.Blockchain import Blockchain
 
-app         = Flask(__name__)
-blockchain  = Blockchain()
-node_identifier = str(uuid4()).replace('-', '') # Generate a globally unique address for this node
+app = Flask(__name__)
+blockchain = Blockchain()
+node_identifier = str(uuid4()).replace(
+    "-", ""
+)  # Generate a globally unique address for this node
+
 
 # #################################################################### #
 #
 #                              GET /mine
 #
 # #################################################################### #
-@app.route('/mine', methods=['GET'])
+@app.route("/mine", methods=["GET"])
 def mine():
-  
-  last_block    = blockchain.last_block
-  last_proof    = last_block['proof']
-  proof         = blockchain.proof_of_work(last_proof)
 
-  blockchain.new_transaction(
-    sender="0",
-    reciever=node_identifier,
-    amount=1,
-  )
-  
-  previous_hash = blockchain.hash(last_block)
-  block         = blockchain.new_block(proof, previous_hash)
+    last_block = blockchain.last_block
+    last_proof = last_block["proof"]
+    proof = blockchain.proof_of_work(last_proof)
 
-  response = {
-    'message': "New Block Forged",
-    'index': block['index'],
-    'transactions': block['transactions'],
-    'proof': block['proof'],
-    'previous_hash': block['previous_hash'],
-  }
+    blockchain.new_transaction(
+        sender="0",
+        reciever=node_identifier,
+        amount=1,
+    )
 
-  return jsonify(response), 200
-  
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        "message": "New Block Forged",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"],
+    }
+
+    return jsonify(response), 200
+
+
 # #################################################################### #
 #
 #                        POST /transactions/new
 #
 # #################################################################### #
 
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-  values      = request.get_json()
-  # required    = ['sender', 'reciever', 'amount']
-  required    = ['sender', 'amount']
-  
-  if not all(field in values for field in required):
-    return 'Missing values', 400
-  
-  index       = blockchain.new_transaction(values['sender'], node_identifier, values['amount'])
-  response    = {'message': 'transaction will be added to Block {}'.format(index)}
 
-  return jsonify(response), 201
+@app.route("/transactions/new", methods=["POST"])
+def new_transaction():
+    values = request.get_json()
+    # required    = ['sender', 'reciever', 'amount']
+    required = ["sender", "amount"]
+
+    if not all(field in values for field in required):
+        return "Missing values", 400
+
+    index = blockchain.new_transaction(
+        values["sender"], node_identifier, values["amount"]
+    )
+    response = {"message": "transaction will be added to Block {}".format(index)}
+
+    return jsonify(response), 201
+
 
 # #################################################################### #
 #
 #                        GET /chain
 #
 # #################################################################### #
-@app.route('/chain', methods=['GET'])
+@app.route("/chain", methods=["GET"])
 def full_chain():
-  response = {
-    'chain': blockchain.chain,
-    'length': len(blockchain.chain),
-  }
-  return jsonify(response), 200
+    response = {
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain),
+    }
+    return jsonify(response), 200
 
 
 # #################################################################### #
@@ -77,23 +87,24 @@ def full_chain():
 #
 # #################################################################### #
 
-@app.route('/nodes/register', methods=['POST'])
+
+@app.route("/nodes/register", methods=["POST"])
 def register_nodes():
-  values      = request.get_json()
-  nodes       = values['nodes']
-  
-  if nodes is None:
-    return 'Error: Please supply with a valid list of nodes', 400
-  
-  for node in nodes:
-    blockchain.register_node(node)
+    values = request.get_json()
+    nodes = values["nodes"]
 
-  response = {
-    'message': 'New nodes have been added',
-    'total_nodes': list(blockchain.nodes)
-  }
+    if nodes is None:
+        return "Error: Please supply with a valid list of nodes", 400
 
-  return jsonify(response), 201
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        "message": "New nodes have been added",
+        "total_nodes": list(blockchain.nodes),
+    }
+
+    return jsonify(response), 201
 
 
 # #################################################################### #
@@ -101,23 +112,20 @@ def register_nodes():
 #                        GET /nodes/resolver
 #
 # #################################################################### #
-@app.route('/nodes/resolve', methods=['GET'])
+@app.route("/nodes/resolve", methods=["GET"])
 def consenious():
-  replaced    = blockchain.resolve_conflict()
+    replaced = blockchain.resolve_conflict()
 
-  if replaced: 
-    response = {
-      'message': 'Our chain has been replaced',
-      'new_chain': blockchain.chain
-    }
+    if replaced:
+        response = {
+            "message": "Our chain has been replaced",
+            "new_chain": blockchain.chain,
+        }
 
-  else:
-    response = {
-      'message': 'Our chain is authoritative',
-      'chain': blockchain.chain
-    }
+    else:
+        response = {"message": "Our chain is authoritative", "chain": blockchain.chain}
 
-  return jsonify(response), 200
+    return jsonify(response), 200
 
 
 # #################################################################### #
@@ -125,12 +133,14 @@ def consenious():
 #                               RUN PROGRAM
 #
 # #################################################################### #
-if __name__ == '__main__':
-  from argparse import ArgumentParser
+if __name__ == "__main__":
+    from argparse import ArgumentParser
 
-  parser = ArgumentParser()
-  parser.add_argument('-p', '--port', default=5000, type=int, help='port to run the program.')
-  args = parser.parse_args()
-  port = args.port
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-p", "--port", default=8080, type=int, help="port to run the program."
+    )
+    args = parser.parse_args()
+    port = args.port
 
-  app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
